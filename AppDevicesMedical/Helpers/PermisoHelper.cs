a9515b0d3ac9ba57e2eeccfc3fc8ce3d8ca1b6bb
Marcos Martinez Valdez <l21210398@tectijuana.edu.mx>
2025-11-15 02:58:08 -0800

    Agregando carpeta Helpers

---
+++ AppDevicesMedical/Helpers/PermisoHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace AppDevicesMedical.Helpers
+{
+    public static class PermisoHelper
+    {
+        public static bool TienePermiso(ClaimsPrincipal user, string permiso)
+        {
+            return user.Claims.Any(c => c.Type == "Permiso" && c.Value == permiso);
+        }
+    }
+
+}
